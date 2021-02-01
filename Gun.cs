02dbe02f592b53003039a3a8dbd17e2080bb777b
SSOVINSKI <ssovinski12@gmail.com>
2021-02-01 19:40:22 +0100

    gra

---
+++ Gun.cs
@@ -0,0 +1,42 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+	public Rigidbody BULLET;
+	public Transform FIRESPOT;
+	public float power;
+
+	private Rigidbody projectile;
+	private bool fire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetButtonDown("Fire1"))
+        {
+        	projectile = Instantiate(BULLET, FIRESPOT.position, transform.rotation);
+        	fire = true;
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+    	if(fire)
+   		{
+   			projectile.velocity = transform.TransformDirection(Vector3.left * power);
+   			fire = false;
+   			Destroy(projectile.gameObject, 3);
+    	}
+     }
+
+
+}
